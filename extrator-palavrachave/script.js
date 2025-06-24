548726aa792b4be30f48d885dd5e1f8b3cbccead
Rafaela Ayres Bulgarelli <rafaela.bulgarelli@escola.pr.gov.br>
2025-06-24 08:00:45 -0300

    uouou

---
+++ extrator-palavrachave/script.js
@@ -0,0 +1,13 @@
+
+const botaoMostraPalavras = document.querySelector("#botao-palavrachave"); 
+
+botaoMostraPalavras.addEventListener("click", mostraPalavrasChave); 
+
+function mostraPalavrasChave() { 
+  const texto = document.querySelector('#entrada-de-texto').value;
+  const campoResultado = document.querySelector('#resultado-palavrachave');
+  const palavrasChave = processaTexto(texto);
+
+
+  campoResultado.textContent = palavrasChave.join(", "); 
+} 
